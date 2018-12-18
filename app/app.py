3fea20d35632d75e71ba6f34aa72b153484da1ab
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-18 22:43:09 +0300

    [Feature #162735723] Added the right method and route

--- app/app.py
+++ app/app.py
@@ -49,8 +49,8 @@ def create_app(config_name):
             return jsonify({"Message": "Question Deleted Successfully"}), 200
         return jsonify({"Message" : "Question with that question_id not found", "Status" : "Error"}), 404
 
-    @app.route("/questions/<int:qsn_id>/answers", methods=['GET'])
-    def all_answers(qsn_id):
+    @app.route("/questions/<int:qsn_id>/answers", methods=['POST'])
+    def post_answer(qsn_id):
         pass
 
     return app 
