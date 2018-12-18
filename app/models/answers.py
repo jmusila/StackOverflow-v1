e61472bf73a79fd8a20679063a4beaa45b464aa5
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-18 22:29:58 +0300

    [Feature #162735723] Added the constructor arguments

--- app/models/answers.py
+++ app/models/answers.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
 class AnswerOp():
-    def __init__(self):
+    def __init__(self, ans_body):
+        self.ans_body = ans_body
+        self.timeposted = datetime.now()
+        self.Answers = []
         pass
