cf45c2052869ffa278f0958bcaee866457844322
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-18 23:10:15 +0300

    [Bug #162742087] Added the for loop to get a single question

--- app/models/questions.py
+++ app/models/questions.py
@@ -21,4 +21,7 @@ class QuestionsOp():
         self.Questions.append(new_qsn)
 
     def get_one_question(self, qsn_id):
+        for i in self.Questions:
+            if i['qsn_id'] == qsn_id:
+                return i
         pass
