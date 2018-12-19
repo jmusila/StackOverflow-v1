028d8173cdca77f9a022afbcf19a72920e71deb0
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-19 12:14:10 +0300

    [Chore #162694283] Added the class and the setup method

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -6,3 +6,8 @@ import json
 from app.app import create_app
 from app.models.questions import QuestionsOp
 from app.app import answer,question
+
+class TestQuestions(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client()
