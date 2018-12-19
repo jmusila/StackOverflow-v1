faacf13028832ea5eee6d943533ed352507eb03b
jonathanmusila <jonathanmusila6@gmail.com>
2018-12-19 13:09:37 +0300

    [Chore #162694296] Added test for posting answer

--- tests/test_answers.py
+++ tests/test_answers.py
@@ -21,3 +21,17 @@ class TestAnswers(unittest.TestCase):
         self.answer = {
             "ans_body": "This is the question body"
         }
+
+    def test_user_can_post_answer(self):
+        """
+        Tests a user can post answer to a question.
+        """
+        response = self.client.post("/questions",
+                                    data=json.dumps(self.question),
+                                    content_type="application/json")
+        response = self.client.post("/questions/1/answers",
+                                    data=json.dumps(self.answer),
+                                    content_type="application/json")
+        self.assertEqual(response.status_code, 201)
+        response_msg = json.loads(response.data.decode("UTF-8"))
+        self.assertEqual("The Answer was added successfully", response_msg["Message"])
